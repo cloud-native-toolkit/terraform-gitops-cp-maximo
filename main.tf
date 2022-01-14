2e121b644c778c364bd5beb1c0a341686607eaac
Thomas Skill <tskill@us.ibm.com>
2022-01-14 16:29:00 -0600

    cleanup-start-sbo

--- main.tf
+++ main.tf
@@ -1,14 +1,10 @@
 locals {
-  name          = "my-module"
+  name          = "ibmmax"
   bin_dir       = module.setup_clis.bin_dir
   yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
-  service_url   = "http://${local.name}.${var.namespace}"
-  values_content = {
-  }
+
   layer = "services"
-  type  = "base"
   application_branch = "main"
-  namespace = var.namespace
   layer_config = var.gitops_config[local.layer]
 }
 
@@ -16,24 +12,27 @@ module setup_clis {
   source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
 }
 
-resource null_resource create_yaml {
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/create-yaml.sh '${local.name}' '${local.yaml_dir}'"
 
+
+resource null_resource patch_sbo {
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/patchSBO.sh '${local.yaml_dir}' "
+    
     environment = {
-      VALUES_CONTENT = yamlencode(local.values_content)
+      BIN_DIR = local.bin_dir
     }
   }
-}
+} 
+
 
 resource null_resource setup_gitops {
-  depends_on = [null_resource.create_yaml]
+  depends_on = [null_resource.patch_sbo]
 
   provisioner "local-exec" {
-    command = "${local.bin_dir}/igc gitops-module '${local.name}' -n '${var.namespace}' --contentDir '${local.yaml_dir}' --serverName '${var.server_name}' -l '${local.layer}' --type '${local.type}' --debug"
+    command = "${local.bin_dir}/igc gitops-module '${local.name}' -n '${var.namespace}' --contentDir '${local.yaml_dir}' --serverName '${var.server_name}' -l '${local.layer}' --debug"
 
     environment = {
-      GIT_CREDENTIALS = yamlencode(var.git_credentials)
+      GIT_CREDENTIALS = yamlencode(nonsensitive(var.git_credentials))
       GITOPS_CONFIG   = yamlencode(var.gitops_config)
     }
   }
