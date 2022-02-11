7397eaad35d3a1fe07bf7f09766e4eb06905eed1
Thomas Skill <tskill@us.ibm.com>
2022-02-11 15:16:09 -0600

    remove nmsp var from test

--- test/stages/stage1-certmgr.tf
+++ test/stages/stage1-certmgr.tf
@@ -4,6 +4,5 @@ module "certmgr" {
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
-  namespace = "cert-manager"
   
 }
