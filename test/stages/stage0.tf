0876fbcf1c0e248e80bfdd1d7592479d6ac03a70
tcskill <tskill@us.ibm.com>
2022-05-25 16:51:17 -0500

    adds provider info

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -1,17 +1,7 @@
 terraform {
   required_providers {
     gitops = {
-      source = "cloud-native-toolkit/gitops"
+      source  = "cloud-native-toolkit/gitops"
     }
   }
-}
-
-provider "gitops" {
-  username = var.git_username
-  token = var.git_token
-  bin_dir  = module.setup_clis.bin_dir
-}
-
-module setup_clis {
-  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
 }
